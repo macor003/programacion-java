379fd59a97bf76944d6f605c4b9a901b01c3073c
Mario Ortega <mcor63@gmail.com>
2017-09-09 15:17:48 -0400

    Actualizado

---
+++ CodeWars/src/main/java/com/macor/CodeWars/toUpperCase.java
@@ -0,0 +1,15 @@
+package com.macor.CodeWars;
+
+public class toUpperCase {
+
+	public static void main(String[] args) {
+		
+		String word = "0365u0010365000001";
+		
+		System.out.println(word.toUpperCase());
+		
+		
+
+	}
+
+}
